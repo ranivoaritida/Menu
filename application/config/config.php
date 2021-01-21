beb5d94a9f488afaa1f6201aa4520d89b0c7b4a3
RANIVOARITIDA Sandy <ranivoaritiadasandy@gmail.com>
2021-01-21 03:08:57 +0300

    2n

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cantine.unaux.com/menu/';
+$config['base_url'] = 'https://cantine-menu.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
