2033c40a84c937eb6aa351afbe9fb37a644fea43
RANIVOARITIDA Sandy <ranivoaritiadasandy@gmail.com>
2021-01-21 04:14:33 +0300

    ok

--- application/models/cantine_model.php
+++ application/models/cantine_model.php
@@ -1,17 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 class Cantine_model extends CI_Model {
-=======
-class cantine_model extends CI_Model {
->>>>>>> 8f7d4ddc636b367c61a1f5066210b613fff93d47
-=======
-class cantine_model extends CI_model {
->>>>>>> 08b5b8a31ca48d162a444586dcf417747043fb71
-=======
-class Cantine_model extends CI_model {
->>>>>>> 198181c1aa88cfa77cc10672bfa02a432668dafd
     public function getPlats($idmenu){
         $sql = "select Plat.id, Plat.intitule, Plat.code, sum(quantite) as quantite
         from Plat join Commande
